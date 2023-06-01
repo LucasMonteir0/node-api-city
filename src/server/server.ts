cb2b7f8b2b811a03199d198025a1a6dfa15daecc
Lucas Monteiro <lucasmonteirob1@outlook.com>
2023-06-01 18:15:38 -0300

    eslint/eidtorconfig

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,8 +2,9 @@ import express from 'express';
 
 const server = express();
 
+
 server.get('/', (req, res) => {
-    return res.send('Hello World');
+  return res.send('Hello World');
 });
 
 export { server };
